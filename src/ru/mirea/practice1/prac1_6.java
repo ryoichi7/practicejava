22c1a08f524ffbd3e2358da05d41d66adf873be8
ryoichi7 <ryoichidoto@gmail.com>
2023-09-07 20:51:44 +0300

    asd

--- src/ru/mirea/practice1/prac1_6.java
+++ src/ru/mirea/practice1/prac1_6.java
@@ -1,9 +1,7 @@
 package ru.mirea.practice1;
 
-import java.util.Scanner;
 public class prac1_6 {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
         double harmonic = 0;
         for (double i = 1; i < 11; ++i){
             harmonic += 1/i;
