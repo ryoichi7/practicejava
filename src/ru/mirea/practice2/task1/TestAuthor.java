9fe7de7c71f4162a65ecc23a3ec3043b4a30d0bf
ryoichi7 <ryoichidoto@gmail.com>
2023-09-09 13:35:26 +0300

    asd

--- src/ru/mirea/practice2/task1/TestAuthor.java
+++ src/ru/mirea/practice2/task1/TestAuthor.java
@@ -1,4 +1,23 @@
 package ru.mirea.practice2.task1;
 
+import java.util.Scanner;
 public class TestAuthor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter author's name");
+        String name = sc.nextLine();
+        System.out.println("Enter author's email");
+        String email = sc.nextLine();
+        System.out.println("Enter author's gender");
+        char gender = sc.nextLine().charAt(0);
+        author Author = new author(name, email, gender);
+        System.out.println("Information about current author:");
+        System.out.printf("Name: %s Email: %s, Gender: %c%n", Author.getName(), Author.getEmail(), Author.getGender());
+        System.out.println("Enter new email");
+        email = sc.nextLine();
+        Author.setEmail(email);
+        System.out.println("Updated information about current author:");
+        System.out.printf("Name: %s Email: %s, Gender: %c%n", Author.getName(), Author.getEmail(), Author.getGender());
+        System.out.println("Class info: " + Author);
+    }
 }
--- src/ru/mirea/practice2/task1/author.java
+++ src/ru/mirea/practice2/task1/author.java
@@ -1,12 +1,29 @@
 package ru.mirea.practice2.task1;
 
 public class author {
-    private String name, email;
-    private char gender;
+
+    author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    private final String name;
+    private String email;
+    private final char gender;
     public String getName() {
         return name;
     }
     public String getEmail(){
         return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public char getGender() {
+        return gender;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
