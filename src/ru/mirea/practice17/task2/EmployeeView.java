df4ade0f9eb0a0dba1d7c3dbb8de79d2f31a3730
ryoichi7 <ryoichidoto@gmail.com>
2023-10-26 18:58:16 +0300

    asd

---
+++ src/ru/mirea/practice17/task2/EmployeeView.java
@@ -0,0 +1,7 @@
+package ru.mirea.practice17.task2;
+
+public class EmployeeView {
+    public void printEmployeeSalary(int Salary, String EmployeeName) {
+        System.out.printf("The salary of employee %s is: %d\n", EmployeeName, Salary);
+    }
+}
