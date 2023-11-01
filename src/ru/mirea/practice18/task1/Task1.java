53ccc18dbedac31af09dc7d75c7b63413bf23f6d
ryoichi7 <ryoichidoto@gmail.com>
2023-11-01 15:35:34 +0300

    practice 18

---
+++ src/ru/mirea/practice18/task1/Task1.java
@@ -0,0 +1,8 @@
+package ru.mirea.practice18.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Exception1 exception1 = new Exception1();
+        exception1.exceptionDemo();
+    }
+}
